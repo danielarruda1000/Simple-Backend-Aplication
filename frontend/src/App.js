b43b08b4404937a9302d3dca4a1a541e6ebd4fbd
Daniel Arruda <danielarruda1000>
2019-08-23 19:50:43 -0300

    create Login screen and using React Route to navegation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
   return (
     <div>
-      <Register/>
+      <Switch>
+        
+        <Route exact path="/" component={Login}/>
+        <Route path="/register" component={Register}/>
+      </Switch>
     </div>
   );
 }
